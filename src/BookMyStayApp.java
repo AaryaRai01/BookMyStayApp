06d717a5bec280dd8dc28f7f1de77bf760b3c2c8
AaryaRai01 <aarya.rai185@gmail.com>
2026-03-22 18:11:09 +0530

    UC3:Centralized Room Inventory Management

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -1,6 +1,87 @@
-public class BookMyStayApp {
-    public static void main(String[] args) {
-        System.out.println();
+import java.util.Scanner;
 
+public class BookMyStayApp {
+
+    protected String roomNumber;
+    protected int numberOfBeds;
+    protected double roomSize;
+    protected double pricePerNight;
+    protected String roomType;
+
+    public BookMyStayApp(String roomNumber, int numberOfBeds, double roomSize, double pricePerNight, String roomType) {
+        this.roomNumber = roomNumber;
+        this.numberOfBeds = numberOfBeds;
+        this.roomSize = roomSize;
+        this.pricePerNight = pricePerNight;
+        this.roomType = roomType;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("\nRoom Details");
+        System.out.println("Room Number   : " + roomNumber);
+        System.out.println("Room Type     : " + roomType);
+        System.out.println("Number of Beds: " + numberOfBeds);
+        System.out.println("Room Size     : " + roomSize + " sqft");
+        System.out.println("Price/Night   : " + pricePerNight);
+        System.out.println("-----------------------------");
+    }
+
+    // UC3: Hotel Room Inventory Status
+    public static void displayInventory() {
+
+        System.out.println("\nHotel Room Inventory Status\n");
+
+        System.out.println("Single Room:");
+        System.out.println("Beds: 1");
+        System.out.println("Size: 250 sqft");
+        System.out.println("Price per night: 1500.0");
+        System.out.println("Available Rooms: 5\n");
+
+        System.out.println("Double Room:");
+        System.out.println("Beds: 2");
+        System.out.println("Size: 400 sqft");
+        System.out.println("Price per night: 2500.0");
+        System.out.println("Available Rooms: 3\n");
+
+        System.out.println("Suite Room:");
+        System.out.println("Beds: 3");
+        System.out.println("Size: 750 sqft");
+        System.out.println("Price per night: 5000.0");
+        System.out.println("Available Rooms: 2\n");
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to Hotel Booking System");
+        System.out.println("System initialized successfully.");
+
+        System.out.print("Enter Room Number: ");
+        String roomNumber = sc.nextLine();
+
+        System.out.print("Enter Room Type (Single/Double/Suite): ");
+        String roomType = sc.nextLine();
+
+        System.out.print("Enter Number of Beds: ");
+        int beds = sc.nextInt();
+
+        System.out.print("Enter Room Size (sq ft): ");
+        double size = sc.nextDouble();
+
+        System.out.print("Enter Price per Night: ");
+        double price = sc.nextDouble();
+
+        BookMyStayApp room = new BookMyStayApp(roomNumber, beds, size, price, roomType);
+
+        System.out.println("\nApplication started successfully.");
+        System.out.println("System is ready to manage hotel bookings.");
+
+        room.displayRoomDetails();
+
+        // UC3 Output
+        displayInventory();
+
+        sc.close();
     }
 }
